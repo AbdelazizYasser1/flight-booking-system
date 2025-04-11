66a521c85892dc513168dcc27cab7a075cee0c4a
Abdelaziz yasser <theknower62@gmail.com>
2025-04-11 17:40:20 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,45 +15,45 @@ use App\Http\Controllers\{
     TransactionPassengerController
 };
 
-Route::post('/register', [AuthController::class, 'register'])->name('register');   //Done 
-Route::post('/login', [AuthController::class, 'login'])->name('login');         //Done       
+Route::post('/register', [AuthController::class, 'register'])->name('register');   
+Route::post('/login', [AuthController::class, 'login'])->name('login');              
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);        //Done     
-    Route::post('/logout', [AuthController::class, 'logout']);                  //Done    
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);            
+    Route::post('/logout', [AuthController::class, 'logout']);                      
 
-    Route::get('/airports', [AirportController::class, 'index']);               //Done   
-    Route::get('/airports/{name}', [AirportController::class, 'show']);         //Done
-    Route::get('/airlines', [AirlineController::class, 'index']);               //Done
-    Route::get('/airlines/{name}', [AirlineController::class, 'show']);         //Done  
+    Route::get('/airports', [AirportController::class, 'index']);                  
+    Route::get('/airports/{name}', [AirportController::class, 'show']);         
+    Route::get('/airlines', [AirlineController::class, 'index']);               
+    Route::get('/airlines/{name}', [AirlineController::class, 'show']);           
 
-    Route::get('/flights', [FlightController::class, 'index']);                 //Done         
-    Route::get('/flights/{flight_number}', [FlightController::class, 'show']);         //Done
-    Route::get('/flight-classes', [FlightClassController::class, 'index']);     //Done
-    Route::get('/flight-seats', [FlightSeatController::class, 'index']);        //Done
-    Route::get('/flight-segments', [FlightSegmentController::class, 'index']);  //Done
+    Route::get('/flights', [FlightController::class, 'index']);                         
+    Route::get('/flights/{flight_number}', [FlightController::class, 'show']);         
+    Route::get('/flight-classes', [FlightClassController::class, 'index']);     
+    Route::get('/flight-seats', [FlightSeatController::class, 'index']);        
+    Route::get('/flight-segments', [FlightSegmentController::class, 'index']);  
 
-    Route::get('/promo-codes', [PromoCodeController::class, 'index']);          //Done
-    Route::get('/promo-codes/{code}', [PromoCodeController::class, 'show']);    //Done
-    Route::get('/validate-promo/{code}', [PromoCodeController::class, 'validatePromoCode']); //Done
+    Route::get('/promo-codes', [PromoCodeController::class, 'index']);          
+    Route::get('/promo-codes/{code}', [PromoCodeController::class, 'show']);    
+    Route::get('/validate-promo/{code}', [PromoCodeController::class, 'validatePromoCode']); 
 
-    Route::get('/facilities', [FacilityController::class, 'index']);            //Done
-    Route::get('/facilities/{name}', [FacilityController::class, 'show']);      //Done
+    Route::get('/facilities', [FacilityController::class, 'index']);            
+    Route::get('/facilities/{name}', [FacilityController::class, 'show']);      
 
     Route::middleware('admin')->group(function () {
-        Route::apiResource('facilities', FacilityController::class)->except(['index', 'show']); //Done
-        Route::apiResource('airports', AirportController::class)->except(['index', 'show']);    //Done
-        Route::apiResource('airlines', AirlineController::class)->except(['index', 'show']);    //Done
-        Route::apiResource('flights', FlightController::class)->except(['index', 'show']);      //Done
-        Route::apiResource('flight-classes', FlightClassController::class)->except(['index']);  //Done
-        Route::apiResource('flight-seats', FlightSeatController::class)->except(['index']);     //Done
-        Route::apiResource('flight-segments', FlightSegmentController::class)->except(['index']);   //Done
+        Route::apiResource('facilities', FacilityController::class)->except(['index', 'show']); 
+        Route::apiResource('airports', AirportController::class)->except(['index', 'show']);    
+        Route::apiResource('airlines', AirlineController::class)->except(['index', 'show']);    
+        Route::apiResource('flights', FlightController::class)->except(['index', 'show']);      
+        Route::apiResource('flight-classes', FlightClassController::class)->except(['index']);  
+        Route::apiResource('flight-seats', FlightSeatController::class)->except(['index']);     
+        Route::apiResource('flight-segments', FlightSegmentController::class)->except(['index']);   
         Route::apiResource('transactions', TransactionController::class);
         Route::apiResource('transaction-passengers', TransactionPassengerController::class);
 
-        Route::post('/promo-codes', [PromoCodeController::class, 'store']);             //Done
-        Route::put('/promo-codes/{promoCode}', [PromoCodeController::class, 'update']);     //Done
-        Route::delete('/promo-codes/{promoCode}', [PromoCodeController::class, 'destroy']);    //Done
+        Route::post('/promo-codes', [PromoCodeController::class, 'store']);             
+        Route::put('/promo-codes/{promoCode}', [PromoCodeController::class, 'update']);     
+        Route::delete('/promo-codes/{promoCode}', [PromoCodeController::class, 'destroy']);    
     });
 });
